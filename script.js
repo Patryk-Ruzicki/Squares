0d79f172e9fdc65c3db147af35e56536c57c1ca2
ArcySanta <arcysanta@gmail.com>
2023-02-03 23:04:06 +0100

    Improved design
    +new fonts from fontello
    +hyperlinks in footer
    +creator nickname
    +fixed info in mobile

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+/*
+document.oncontextmenu = () => {
+    return false;
+}
+document.onkeydown = e => {
+    if (e.key == "F12") {
+        return false;
+    }
+    if (e.ctrlKey && e.key == "u") {
+        return false;
+    }
+    if (e.ctrlKey && e.key == "c") {
+        return false;
+    }
+    if (e.ctrlKey && e.key == "v") {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == "i") {
+        return false;
+    }
+}   
+*/
+
 let timeRandom, i = 1;    
 timeRandom = Math.floor(Math.random()*10+1);
 
 console.log(timeRandom);
 
+
+//delating info paragraph
 setTimeout(() => {
     const paraInfo = document.querySelector('#info');
     paraInfo.style.opacity = 0;
-}, 5000);
+}, 3000);
 
 paraInfo.style.display = 'none';
